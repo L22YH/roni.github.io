80c064b9e1934ed8fb305f4d1aa8cdc586ddfec9
L22YH <dudgkr666@naver.com>
2023-08-30 04:10:30 +0900

    adding my files to my repository

---
+++ web_Project/Roni_BirthDay/gift.js
@@ -0,0 +1,26 @@
+// ------------Gift Box 1 Start Here------------
+var giftImage1 = document.createElement("img");
+var src1 = document.getElementById("present");
+
+// gift box
+giftImage1.src = "https://s3-us-west-2.amazonaws.com/s.cdpn.io/1083533/gift-icon.png";
+
+// For middle Gap in Imagㄹe
+giftImage1.style.margin = "10px";
+
+//appent child
+src1.appendChild(giftImage1);
+
+
+//After Clicked open This surprise
+giftImage1.addEventListener('click', function () {
+    giftImage1.src = "1.gif";
+})
+// ------------Gift Box 1 End Here------------
+
+//Title change
+var allImage = giftImage1 && giftImage2 && giftImage3 && giftImage4 && giftImage5;
+allImage.addEventListener('click', function () {
+    var mainTitle = document.getElementById("title");
+    mainTitle.innerHTML = "Enjoy your present!";
+})
